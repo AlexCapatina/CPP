89b014fb9d1481c64c4fbb912658c916636b879c
Alex <oravetzal@yahoo.com>
2023-02-26 00:05:59 -0500

    Trying compile time and runtime errors

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
+//This brings in the iostream library
 #include <iostream>
 
-int main(){    
+int main(){   
+    //This is going to print the strings 
     std::cout << "Number 1" << std::endl;
     std::cout << "Number 2" << std::endl;
     std::cout << "Number 3" << std::endl;
+
+    //Runtime error
+    int value = 7/0;
+    std::cout << "value: " << value << std::endl;
 
     return 0;
 }
