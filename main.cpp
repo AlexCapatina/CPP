d621ea13e3cd8e7d6dba85cbbde0a45854cf054f
Alex <oravetzal@yahoo.com>
2023-03-07 10:31:10 -0500

    Played with basic functions

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 //This brings in the iostream library
 #include <iostream>
 
-int main(){   
-    //This is going to print the strings 
-    std::cout << "Number 1" << std::endl;
-    std::cout << "Number 2" << std::endl;
-    std::cout << "Number 3" << std::endl;
+int addNumbers(int first_param, int second_param){
+    int result = first_param + second_param;
+    return result;
+}
 
-    //Runtime error
-    // int value = 7/0;
-    // std::cout << "value: " << value << std::endl;
+int multiplyNumbers(int first_param, int second_param){
+    int result = first_param * second_param;
+    return result;
+}
+
+int main(){   
+    int first_number {3}; //Staement
+    int second_number {7};
+
+    std::cout << "First number: " << first_number << std::endl;
+    std::cout << "Second number: " << second_number << std::endl;    
+
+    int sum = first_number + second_number;
+    std::cout << "Sum 1: " << sum << std::endl;
+
+    sum = addNumbers(12, 13);
+    std::cout << "Sum 2: " << sum << std::endl;
+
+    sum = addNumbers(12, 14);
+    std::cout << "Sum 3: " << sum << std::endl;
+
+    std::cout << "Sum 4: " << addNumbers(14, 16) << std::endl;
+
+    std::cout << "Multiplication 1: " << multiplyNumbers(4,5) << std::endl;
 
     return 0;
 }
